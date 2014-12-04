4d284cdc05e0453902bc10feaf0d956364a78fc2
Aravind Senguttuvan <araving.aravind@gmail.com>
2014-12-03 19:09:14 -0700

    Notification done

--- src/com/example/dntmoi/MainActivity.java
+++ src/com/example/dntmoi/MainActivity.java
@@ -1,38 +1,27 @@
 package com.example.dntmoi;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import com.example.dntmoi.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
-import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener {
+public class MainActivity extends FragmentActivity implements
+		ConnectionCallbacks, OnConnectionFailedListener {
 	private GoogleMap map;
 	private LatLng ltlng;
 	private LocationClient mLocationClient;
@@ -42,8 +31,8 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 	private static final float ACCURACY = 3.0f;
 	private Location mCurrentLocation;
 
-    LocationManager lm;
-    
+	LocationManager lm;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,9 +40,9 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 		InitialiseMap();
 		Button buttonreload1 = (Button) findViewById(R.id.buttonreload);
 		Button buttonreload2 = (Button) findViewById(R.id.buttonreload2);
-		
+
 		mLocationClient = new LocationClient(this, this, this);
-		
+
 		buttonreload1.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -66,49 +55,54 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 			}
 
 		});
-		
+
 		buttonreload2.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				/*
-				 * // TODO Auto-generated method stub map.clear(); ltlng = new
-				 * LatLng(75, 65); map.addMarker(new
-				 * MarkerOptions().position(ltlng).title( "Hello world2:"));
-				 */
-								
-
+				notification();
 			}
-	
-		});
-		
-	}
-	/*
-     * Called when the Activity becomes visible.
-     */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Connect the client.
-        mLocationClient.connect();
-    }
 
-    /*
-     * Called when the Activity is no longer visible.
-     */
-    @Override
-    protected void onStop() {
-        // Disconnecting the client invalidates it.
-        //mLocationClient.disconnect();
-        super.onStop();
-    }
-    
+		});
+
+	}
+
+	/*
+	 * Called when the Activity becomes visible.
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+	}
+
+	/*
+	 * Called when the Activity is no longer visible.
+	 */
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		// mLocationClient.disconnect();
+		super.onStop();
+	}
+
 	public void alertBox(String text) {
 		AlertDialog.Builder alert = new AlertDialog.Builder(this);
 		alert.setTitle("Info");
 		alert.setMessage(text);
 		alert.setPositiveButton("OK", null);
 		alert.show();
+	}
+
+	public void notification() {
+		int mNotificationId = 001;
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+				this).setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("My notification")
+				.setContentText("Hello World!");
+		NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		mNotifyMgr.notify(mNotificationId, mBuilder.build());
 	}
 
 	private void InitialiseMap() {
@@ -125,21 +119,23 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 			for (int i = 60; i < 70; i++) {
 				ltlng = new LatLng(i, i + 30);
 
-				//map.addMarker(new MarkerOptions().position(ltlng).title("Hello world:" + i));
+				// map.addMarker(new
+				// MarkerOptions().position(ltlng).title("Hello world:" + i));
 			}
 		}
 	}
+
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void onConnected(Bundle arg0) {
 		// TODO Auto-generated method stub
 		mLocationClient.setMockMode(true);
-		Location newLocation = new Location(
-				MainActivity.PROVIDER);
+		Location newLocation = new Location(MainActivity.PROVIDER);
 		double latitude = 55, longitude = 65;
 		float accuracy = 3.0f;
 		newLocation.setLatitude(latitude);
@@ -147,17 +143,18 @@ public class MainActivity extends FragmentActivity implements ConnectionCallback
 		newLocation.setAccuracy(accuracy);
 		newLocation.setTime(System.currentTimeMillis());
 
-	    int sdk = android.os.Build.VERSION.SDK_INT;
-	    if(sdk >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
-	        newLocation.setElapsedRealtimeNanos(
-	             SystemClock.elapsedRealtimeNanos());
-	    }
+		int sdk = android.os.Build.VERSION.SDK_INT;
+		if (sdk >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			newLocation.setElapsedRealtimeNanos(SystemClock
+					.elapsedRealtimeNanos());
+		}
 		mLocationClient.setMockLocation(newLocation);
 		alertBox("Mocked !!");
 	}
+
 	@Override
 	public void onDisconnected() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
